d3be8ea3ec353ed1badc19215714a5f4dce9d32e
Jhuliana <jholeanasantos@gmail.com>
2022-07-08 19:53:00 -0300

    feat: little changes

--- src/userContext/UserContextProvider.js
+++ src/userContext/UserContextProvider.js
@@ -2,10 +2,11 @@ import React from "react";
 import { UserContext } from "./userContext.js";
 import { useState } from "react";
 export default function UserContextProvider(props) {
-  const URL = "https://blaze-comics.herokuapp.com";
+  const URL = "http://localhost:5000";
   const [token, setToken] = useState();
+  const [bodyCart, setBodyCart] = useState();
   return (
-    <UserContext.Provider value={{ URL, token, setToken }}>
+    <UserContext.Provider value={{ URL, token, setToken, bodyCart, setBodyCart }}>
       {props.children}
     </UserContext.Provider>
   );
